4142379ba4f2017a9b4aeb1df07f0c596f5e750b
Sanjay Srivallabh Singapuram <t-sansri@microsoft.com>
2019-01-14 14:34:23 +0800

    Timing test

--- tests/test_math_simpleTS.cpp
+++ tests/test_math_simpleTS.cpp
@@ -1,5 +1,15 @@
+#include <chrono>
+
 #include "math.h"
 #include "matrix.h"
+
+#define TICK() auto ____NOW____ = std::chrono::system_clock::now();
+#define TOCK()                                                            \
+  {                                                                       \
+    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>( \
+        std::chrono::system_clock::now() - ____NOW____);                  \
+    std::cout << elapsed.count() << "ms" << std::endl;                    \
+  }
 
 int main(int argc, char* argv[]) {
   CHECK(argc == 3, "Usage: ./a.out A_file.mtx B_file.mtx");
@@ -12,24 +22,28 @@ int main(int argc, char* argv[]) {
 
   {
     std::unique_ptr<DenseMatrix<double>> b_clone = std::move(b->clone());
+    TICK();
     math::SimpleSparseTriangularSolve<double>(A.get(), b_clone.get());
-    for (size_t i = 0; i < b_clone->rows(); ++i) {
+    /*for (size_t i = 0; i < b_clone->rows(); ++i) {
       for (size_t j = 0; j < b_clone->cols(); ++j) {
         std::cout << *(b_clone->access(i, j)) << ' ';
       }
       std::cout << '\n';
-    }
+    }*/
+    TOCK();
   }
 
   {
     std::unique_ptr<DenseMatrix<double>> b_clone = std::move(b->clone());
+    TICK();
     math::Opt1SparseTriangularSolve<double>(A.get(), b_clone.get());
-    for (size_t i = 0; i < b_clone->rows(); ++i) {
+    /*for (size_t i = 0; i < b_clone->rows(); ++i) {
       for (size_t j = 0; j < b_clone->cols(); ++j) {
         std::cout << *(b_clone->access(i, j)) << ' ';
       }
       std::cout << '\n';
-    }
+    }*/
+    TOCK();
   }
   return 0;
 }
